08b7faed90de13add5aaecd8f15e95d583a634e1
Giovanni Fernandes <giofcosta@gmail.com>
2024-05-05 00:04:29 -0300

    Fix jest configuration

--- tests/home.test.tsx
+++ tests/home.test.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import ContentList from '@/components/ContentList';
-import { mockFetch } from '@/utils/mock-fetch';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ContentList from "@/components/ContentList";
+import { mockFetch } from "@/utils/mock-fetch";
 
-window.fetch = mockFetch([]);
+window.fetch = mockFetch([
+  {
+    id: "1",
+    image: "http://picsum.photos/image",
+    title: "Title",
+    subTitle: "Subtitle",
+    description: "Description",
+    author: "Author",
+    commentsCount: 5,
+  },
+]);
 
-describe('Login component tests', () => {
-
+describe("ContentList component tests", () => {
   beforeEach(() => {
     // write someting before each test
   });
@@ -15,13 +24,11 @@ describe('Login component tests', () => {
     // write someting after each test
   });
 
-  it('Renders correctly initial document', async () => {
-    render(
-      <ContentList />
-    );
-    const loginLabel = screen.getByText('Content Feed');
-
-    expect(loginLabel).toBeInTheDocument();
+  it("Renders correctly initial document", async () => {
+    render(await ContentList());
+    expect(screen.getByText("Title")).toBeInTheDocument();
+    expect(screen.getByText("Subtitle")).toBeInTheDocument();
+    expect(screen.getByText("Description")).toBeInTheDocument();
+    expect(screen.getByText("Author")).toBeInTheDocument();
   });
-
 });
